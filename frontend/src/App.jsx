24955fd99374b72e48c7b4a3d5fdbe4f731a9a72
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-11-02 21:47:56 +0200

    edit chart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,32 @@ import { BarChart } from '@mui/x-charts/BarChart';
 
 function App() {
   const [count, setCount] = useState(0)
+  const data = [
+    { name: 'ТВ-21', атестовані: 25, неАтестовані: 5 },
+    { name: 'ТВ-22', атестовані: 27, неАтестовані: 3 },
+    { name: 'ТВ-23', атестовані: 15, неАтестовані: 12 },
+  ];
+  
+  const series = [
+    {
+      label: 'Атестовані',
+      data: data.map(item => item.атестовані),
+    },
+    {
+      label: 'Неатестовані',
+      data: data.map(item => item.неАтестовані),
+    },
+  ];
+  
+  const categories = data.map(item => item.name);
 
   return (
+    
+    
     <BarChart
-      series={[
-        { data: [35, 44, 24, 34] },
-        { data: [51, 6, 49, 30] },
-        { data: [15, 25, 30, 50] },
-        { data: [60, 50, 15, 25] },
-      ]}
-      height={290}
-      xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+      series={series}
+      height={360}
+      xAxis={[{ data: categories, scaleType: 'band' }]}
       margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
     />
   )
