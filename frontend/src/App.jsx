c1155b3632e50fd693e371d584201c6dbb7089f2
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-11-02 13:16:51 +0200

    create vite-react project, added mui materials

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BarChart } from '@mui/x-charts/BarChart';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BarChart
+      series={[
+        { data: [35, 44, 24, 34] },
+        { data: [51, 6, 49, 30] },
+        { data: [15, 25, 30, 50] },
+        { data: [60, 50, 15, 25] },
+      ]}
+      height={290}
+      xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+    />
+  )
+}
+
+export default App
