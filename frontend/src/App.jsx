b17df472a6767267a9dfbbe599ff6cabddb6342b
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-11-02 22:37:36 +0200

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-import { BarChart } from '@mui/x-charts/BarChart';
+import { Routes, Route } from 'react-router-dom'
+import { StudentPage } from './pages/StudentPage.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const data = [
-    { name: 'ТВ-21', атестовані: 25, неАтестовані: 5 },
-    { name: 'ТВ-22', атестовані: 27, неАтестовані: 3 },
-    { name: 'ТВ-23', атестовані: 15, неАтестовані: 12 },
-  ];
-  
-  const series = [
-    {
-      label: 'Атестовані',
-      data: data.map(item => item.атестовані),
-    },
-    {
-      label: 'Неатестовані',
-      data: data.map(item => item.неАтестовані),
-    },
-  ];
-  
-  const categories = data.map(item => item.name);
-
-  return (
-    
-    
-    <BarChart
-      series={series}
-      height={360}
-      xAxis={[{ data: categories, scaleType: 'band' }]}
-      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-    />
+  return(
+    <>
+    <Routes>
+          <Route path="/student" element={<StudentPage />} />
+    </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/pages/StudentPage.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { useState } from 'react';
+
+
+export const StudentPage = () => {
+    const [count, setCount] = useState(0)
+  const data = [
+    { name: 'ТВ-21', атестовані: 25, неАтестовані: 5 },
+    { name: 'ТВ-22', атестовані: 27, неАтестовані: 3 },
+    { name: 'ТВ-23', атестовані: 15, неАтестовані: 12 },
+  ];
+  
+  const series = [
+    {
+      label: 'Атестовані',
+      data: data.map(item => item.атестовані),
+    },
+    {
+      label: 'Неатестовані',
+      data: data.map(item => item.неАтестовані),
+    },
+  ];
+  
+  const categories = data.map(item => item.name);
+
+  return (
+    <>
+    <p>ssddd</p>
+    
+    <BarChart
+      series={series}
+      height={360}
+      xAxis={[{ data: categories, scaleType: 'band' }]}
+      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+    />
+    </>
+  )
+}
+
+/*
+ const [count, setCount] = useState(0)
+  const data = [
+    { name: 'ТВ-21', атестовані: 25, неАтестовані: 5 },
+    { name: 'ТВ-22', атестовані: 27, неАтестовані: 3 },
+    { name: 'ТВ-23', атестовані: 15, неАтестовані: 12 },
+  ];
+  
+  const series = [
+    {
+      label: 'Атестовані',
+      data: data.map(item => item.атестовані),
+    },
+    {
+      label: 'Неатестовані',
+      data: data.map(item => item.неАтестовані),
+    },
+  ];
+  
+  const categories = data.map(item => item.name);
+
+  return (
+    
+    
+    <BarChart
+      series={series}
+      height={360}
+      xAxis={[{ data: categories, scaleType: 'band' }]}
+      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+    />
+  )
+*/
