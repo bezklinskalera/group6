23a91bb564416f028f442059662393c4220b7c0f
Mary <polishchuk.mariyatv21@gmail.com>
2024-11-04 15:51:10 +0200

    підключення Mongo DB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,12 @@
 import express from 'express';
-import { connectToDb, getDb } from './db.js';
-
 const app = express();
 
-let db;
+app.get("/products", (req, res) => {});
 
-connectToDb((err) => {
-    if (!err) {
-        app.listen(8084, (err) => {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Server Ok + ");
-        });
-        db = getDb();
-    } else {
-        console.log(`DB connection error: ${err}`);
+app.listen(8084, (err) => {
+    if (err) {
+        return console.log(err);
     }
+
+    console.log("Server Ok");
 });
