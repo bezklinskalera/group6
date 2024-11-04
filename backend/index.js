a2a47c4e9f0b43df867b83fd28815c8bad230501
Artem Lazarchuk <la.artem.ser@gmail.com>
2024-11-04 13:46:31 +0200

    Підключення mongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import { connectToDb, getDb } from './db.js';
 
 const app = express();
 
-app.listen(8084, (err) => {
-    if (err) {
-        return console.log(err);
-    }
+let db;
 
-    console.log("Server Ok");
+connectToDb((err) => {
+    if (!err) {
+        app.listen(8084, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Server Ok + ");
+        });
+        db = getDb();
+    } else {
+        console.log(`DB connection error: ${err}`);
+    }
 });
