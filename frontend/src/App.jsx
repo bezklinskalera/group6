1e18755fab6414ffe598ecb9804f50320eaec344
sdzmnk <sdzmnk@gmail.com>
2024-11-06 09:33:43 +0200

    delete signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { StudentPage } from './pages/StudentPage.jsx'
-import { RegistrationPage } from './pages/SignUp.jsx'
 
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <>
     <Routes>
           <Route path="/student" element={<StudentPage />} />
-          <Route path="/signup" element={<RegistrationPage />} />
     </Routes>
     </>
   )
