d80197649cbc6c54ab5879ac3604a1d004b1008f
Elizaveta3 <voronkova.lz2005@gmail.com>
2024-11-19 20:40:27 +0200

    created teacher page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { StudentPage } from './pages/StudentPage.jsx'
+import { TeacherPage } from './pages/TeacherPage.jsx'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Routes>
           <Route path="/student" element={<StudentPage />} />
+          <Route path="/teacher" element={<TeacherPage />} />
     </Routes>
     </>
   )
